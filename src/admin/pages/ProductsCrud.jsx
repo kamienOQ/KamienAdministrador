82767ad3c968db25ad247ef0b4c9068eff5980b5
Victor <Victorjulio345@gmail.com>
2023-03-08 15:36:30 -0600

    Login y cascarón

---
+++ src/admin/pages/ProductsCrud.jsx
@@ -0,0 +1,8 @@
+import BottomNav from "../Components/BottomNav"
+export const ProductsCrud = () => {
+  return (
+    <>
+      <BottomNav/>
+    </>
+  )
+}
---
+++ src/admin/pages/index.js
@@ -0,0 +1,6 @@
+export * from "./OrdersManagementPage";
+export * from "./AdminCrud";
+export * from "./ProfilePage";
+export * from "./AboutCrud";
+export * from "./ProductsCrud";
+export * from "./CategoryCrud";
