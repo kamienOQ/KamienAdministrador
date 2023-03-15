ef18f2c8e73b974ebe324d728826ad88008e81eb
Jose Hidalgo <hidalgochjc@gmail.com>
2023-03-14 23:42:10 -0600

    Develop Branch Merge, Nav Bara and Side bar actualization

--- src/admin/pages/index.js
+++ src/admin/pages/index.js
@@ -1,6 +1,5 @@
 export * from "./OrdersManagementPage";
 export * from "./AdminCrud";
 export * from "./ProfilePage";
-export * from "./AboutCrud";
 export * from "./ProductsCrud";
 export * from "./CategoryCrud";
--- src/admin/routes/AdminRoutes.jsx
+++ src/admin/routes/AdminRoutes.jsx
@@ -1,6 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Navbar } from "../components/Navbar";
-import { AboutCrud, AdminCrud, CategoryCrud, OrdersManagementPage, ProductsCrud, ProfilePage } from "../pages/";
+import { AdminCrud, CategoryCrud, OrdersManagementPage, ProductsCrud, ProfilePage } from "../pages/";
 
 export const AdminRoutes = () => {
   return (
@@ -11,7 +11,6 @@ export const AdminRoutes = () => {
         <Route path="/" element={<OrdersManagementPage />} />
         <Route path="/Administradores" element={<AdminCrud/>} />
         <Route path= "/Perfil" element = {<ProfilePage/>}/>
-        <Route path= "/About" element = {<AboutCrud/>}/>
         <Route path= "/Producto" element = {<ProductsCrud/>}/>
         <Route path= "/Categorias" element = {<CategoryCrud/>}/>
 
